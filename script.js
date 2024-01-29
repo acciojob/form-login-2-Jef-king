ff07c869b3c1a2d78cc265c32a3f022e5a3c0bbb
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-01-29 11:37:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 //your JS code here. If required.
-function display() {alert("
-	'First Name:'+document.getElementById('fname').value");
+function display() {alert(
+	'First Name: '+document.getElementById('fname').value+
+'Last Name: '+document.getElementById('lname').value+
+'Phone Number: '+document.getElementById('pnumber').value+
+'Email ID: '+document.getElementById('email').value);
 }
